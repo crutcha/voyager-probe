aa223b7d49014c4511538981cfbb5340ffa4d452
Andrew Crutchfield <andrewjcrutchfield@gmail.com>
2020-08-02 13:57:09 -0400

    lookup response test

--- icmp_test.go
+++ icmp_test.go
@@ -41,3 +41,24 @@ func TestIcmpCleanupRemoveKey(t *testing.T) {
 	assert.Equal(false, test1ok, "test-1 key removed")
 	assert.Equal(true, test2ok, "test-2 key was not removed")
 }
+
+func TestResponseLookup(t *testing.T) {
+	assert := assert.New(t)
+	response1 := ICMPResponse{
+		Timestamp: time.Now().Add(time.Duration(-2) * time.Minute),
+	}
+	response2 := ICMPResponse{
+		Timestamp: time.Now().Add(time.Duration(-15) * time.Second),
+	}
+	received = ResponseMap{responses: map[string]ICMPResponse{
+		"test-1": response1,
+		"test-2": response2,
+	}}
+
+	value1, lookupErr1 := lookupResponses("test-1")
+	_, lookupErr2 := lookupResponses("test-3")
+	assert.Equal(value1, response1, "lookup failed")
+	assert.Equal(nil, lookupErr1, "lookup err is nill")
+	assert.EqualError(lookupErr2, "Response lookup timed out: test-3")
+
+}
